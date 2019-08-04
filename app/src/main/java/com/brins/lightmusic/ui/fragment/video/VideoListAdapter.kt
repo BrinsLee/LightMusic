5c68c1255c9219d638a2eebcff4b2f189d60ee64
LeePeiLin <747127514@qq.com>
2019-08-04 16:16:26 +0800

    提交：视频播放基本实现

---
+++ app/src/main/java/com/brins/lightmusic/ui/fragment/video/VideoListAdapter.kt
@@ -0,0 +1,77 @@
+package com.brins.lightmusic.ui.fragment.video
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import cn.jzvd.Jzvd
+import com.brins.lightmusic.LightMusicApplication
+import com.brins.lightmusic.R
+import com.brins.lightmusic.model.musicvideo.Mv
+import com.brins.lightmusic.player.VideoStateListener
+import com.brins.lightmusic.ui.base.adapter.OnItemClickListener
+import com.brins.lightmusic.ui.customview.JZVideoPalyerView
+import com.bumptech.glide.Glide
+
+class VideoListAdapter(var MvData: MutableList<Mv>, var context: Context) :
+    RecyclerView.Adapter<VideoListAdapter.ViewHolder>() {
+
+    private var mItemClickListener: OnItemClickListener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context)
+            .inflate(R.layout.item_video_list, parent, false)
+        val viewHolder = ViewHolder(view)
+        return viewHolder
+    }
+
+    override fun getItemCount(): Int {
+        return MvData.size
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        if (MvData.isNotEmpty()) {
+            val mv = MvData[position].data
+            val metaData = MvData[position].metaData
+            holder.title.text = mv.name
+            holder.nickName.text = mv.artistName
+            holder.watchCount.text = if (mv.playCount > 1000) "${mv.playCount / 1000}万播放" else {
+                "${mv.playCount}播放"
+            }
+            holder.videoPlayer.setAllControlsVisiblity(GONE, GONE, VISIBLE, GONE, VISIBLE, GONE, GONE)
+            Glide.with(LightMusicApplication.getLightApplication()).load(mv.cover)
+                .into(holder.videoPlayer.thumbImageView)
+            holder.videoPlayer.setUp(metaData.url, mv.name, Jzvd.SCREEN_NORMAL)
+            holder.videoPlayer.titleTextView.text = ""
+
+            if (mItemClickListener != null) {
+                holder.videoPlayer.setOnClickListener {
+                    mItemClickListener!!.onItemClick(position)
+                }
+            }
+        }
+
+    }
+
+    fun setOnItemListener(listener: OnItemClickListener) {
+        mItemClickListener = listener
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var title: TextView = view.findViewById(R.id.tv_title)
+        var videoPlayer: JZVideoPalyerView = view.findViewById(R.id.video_player)
+        var watchCount: TextView = view.findViewById(R.id.tv_watch_count)
+        var avatar: ImageView = view.findViewById(R.id.iv_avatar)
+        var nickName: TextView = view.findViewById(R.id.tv_author)
+        var commentCount: TextView = view.findViewById(R.id.tv_comment_count)
+    }
+}
